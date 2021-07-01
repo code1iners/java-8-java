ec6c8f9069c9c8d5eb71bddb36e5a96585d5a224
Codeliner <codeliner96@gmail.com>
2021-07-01 19:50:49 +0900

    Updated functional interface.

--- src/main/java/hello/codeliner/java8/functional_interface/FunctionalInterfaceNoArg.java
+++ src/main/java/hello/codeliner/java8/functional_interface/FunctionalInterfaceNoArg.java
@@ -1,11 +1,6 @@
 package main.java.hello.codeliner.java8.functional_interface;
 
-/**
- * @implSpec
- * 함수형 인터페이스.
- * 추상 메서드가 하나만 존재.
- * @FunctionalInterface 어노태이션을 명시함으로써 좀 더 견고하게 관리할 수 있음.
- */
+
 @FunctionalInterface
 public interface FunctionalInterfaceNoArg {
 
--- src/main/java/hello/codeliner/java8/functional_interface/Runner.java
+++ src/main/java/hello/codeliner/java8/functional_interface/Runner.java
@@ -1,5 +1,14 @@
 package main.java.hello.codeliner.java8.functional_interface;
 
+/**
+ * @implSpec
+ * 함수형 인터페이스.
+ * 추상 메서드가 하나만 존재.
+ * @FunctionalInterface 어노태이션을 명시함으로써 좀 더 견고하게 관리할 수 있음.
+ * 함수를 (First object class) 로 사용할 수 있다.
+ * 순수 함수(Pure function) : 사이드 이펙트를 만들 수 없으며, 상태가 없다.
+ * 고차 함수(High-Order function) : 함수가 함수를 매개변수로 받을 수 있고 함수를 리턴할 수 있다.
+ */
 public class Runner {
     public static void main(String[] args) {
 
